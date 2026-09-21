db4802c9651c24f11b87a0bac4c369c60aa10522
MD Rejwan <rejwan7010@gmail.com>
2026-09-22 01:52:00 +0600

    added team listing and team details

--- backend/src/routes/teamRoutes.js
+++ backend/src/routes/teamRoutes.js
@@ -4,6 +4,111 @@ const { requireAuth } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
+router.get("/my", requireAuth, (req, res) => {
+    const userId = req.session.userId;
+    const sql = `
+    SELECT
+        t.team_id,
+        t.team_name,
+        t.team_tag,
+        t.status,
+        g.game_name,
+        tm.role,
+        tm.is_substitute,
+        tm.is_active
+    FROM TEAM_MEMBER tm
+    JOIN TEAM t
+        ON tm.team_id = t.team_id
+    JOIN GAME_ACCOUNT ga
+        ON tm.game_account_id = ga.account_id
+    JOIN GAME g
+        ON t.game_id = g.game_id
+    WHERE ga.user_id = ?
+    ORDER BY t.created_at DESC
+`;
+db.query(
+    sql,
+    [userId],
+    (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({
+                error: "FAILED TO FETCH MY TEAMS"
+            });
+        }
+
+        return res.status(200).json({
+            teams: results
+        });
+    }
+);
+});
+
+router.get("/:id", requireAuth, (req, res) => {
+    const teamId = req.params.id;
+    const sql = `
+    SELECT
+        t.team_id,
+        t.team_name,
+        t.team_tag,
+        t.status,
+        g.game_name,
+        ga.account_id,
+        ga.game_username,
+        tm.role,
+        tm.is_substitute,
+        tm.is_active
+    FROM TEAM t
+    JOIN GAME g
+        ON t.game_id = g.game_id
+    JOIN TEAM_MEMBER tm
+        ON t.team_id = tm.team_id
+    JOIN GAME_ACCOUNT ga
+        ON tm.game_account_id = ga.account_id
+    WHERE t.team_id = ?
+    ORDER BY tm.role = 'captain' DESC, tm.is_substitute ASC;
+`;
+db.query(
+    sql,
+    [teamId],
+    (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({
+                error: "FAILED TO FETCH TEAM"
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                error: "TEAM NOT FOUND"
+            });
+        }
+
+        const team = {
+    team_id: results[0].team_id,
+    team_name: results[0].team_name,
+    team_tag: results[0].team_tag,
+    status: results[0].status,
+    game_name: results[0].game_name,
+    members: []
+};
+results.forEach((row) => {
+    team.members.push({
+        account_id: row.account_id,
+        game_username: row.game_username,
+        role: row.role,
+        is_substitute: row.is_substitute,
+        is_active: row.is_active
+    });
+});
+return res.status(200).json({
+    team: team
+});
+    }
+);
+});
+
 router.post("/", requireAuth, (req, res) => {
     const userId = req.session.userId;
 
